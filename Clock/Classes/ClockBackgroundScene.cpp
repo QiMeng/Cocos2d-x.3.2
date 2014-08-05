ec221348c9159da0b37acc3f7d7b71b1baa402b9
QiMeng <616684889@qq.com>
2014-08-05 11:22:21 +0800

    clock

--- Clock/Classes/ClockBackgroundScene.cpp
+++ Clock/Classes/ClockBackgroundScene.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "ClockBackgroundScene.h"
+
+USING_NS_CC;
+
+Scene * ClockBackgroundScene:: createScene()
+{
+    auto scene = Scene::create();
+    
+    auto layer = ClockBackgroundScene::create();
+    
+    scene->addChild(layer);
+    
+    return scene;
+}
+
+bool ClockBackgroundScene::init()
+{
+    if (!Layer::init()) {
+        return false;
+    }
+    
+    Size winSize = Director::getInstance()->getWinSize();
+    Sprite * clockBk = Sprite::create("background.jpg");
+    clockBk->setPosition(winSize.width/2.0, winSize.height/2.0);
+    clockBk->setScale(.5);
+    this->addChild(clockBk);
+    
+    return true;
+}
+
+
--- Clock/Classes/ClockBackgroundScene.h
+++ Clock/Classes/ClockBackgroundScene.h
@@ -10,5 +10,20 @@
 #define __Clock__ClockBackgroundScene__
 
 #include <iostream>
+#include "cocos2d.h"
+
+class ClockBackgroundScene : public cocos2d::Layer
+{
+public:
+    
+    static cocos2d::Scene* createScene();
+    
+    virtual bool init();
+    
+    CREATE_FUNC(ClockBackgroundScene);
+    
+};
+
+
 
 #endif /* defined(__Clock__ClockBackgroundScene__) */
