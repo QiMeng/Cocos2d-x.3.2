13f416e8cbab0d4ff88f670590bb5c3184bb52d3
QiMeng <616684889@qq.com>
2014-08-05 09:37:16 +0800

    base

---
+++ Clock/Classes/ClockBackgroundScene.cpp
@@ -0,0 +1,9 @@
+//
+//  ClockBackgroundScene.cpp
+//  Clock
+//
+//  Created by strongsoft on 14-8-5.
+//
+//
+
+#include "ClockBackgroundScene.h"
---
+++ Clock/Classes/ClockBackgroundScene.h
@@ -0,0 +1,14 @@
+//
+//  ClockBackgroundScene.h
+//  Clock
+//
+//  Created by strongsoft on 14-8-5.
+//
+//
+
+#ifndef __Clock__ClockBackgroundScene__
+#define __Clock__ClockBackgroundScene__
+
+#include <iostream>
+
+#endif /* defined(__Clock__ClockBackgroundScene__) */
